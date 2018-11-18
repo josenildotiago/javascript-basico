c8ea946e91ef2fc623994dacc6e2ba360a2478c2
Josenildo Tiago <josenildo.tiago.designer@gmail.com>
2018-11-18 01:29:23 -0200

    finalizado aula

--- aula3-operacoes/js/aula.js
+++ aula3-operacoes/js/aula.js
@@ -22,3 +22,21 @@ function diminuir(){
     document.getElementById('campo3').value='';
     document.getElementById('campo4').value='';
 }
+function multiplicar(){
+    var campo1 = parseInt(document.getElementById("campo5").value);
+    var campo2 = parseInt(document.getElementById("campo6").value);
+
+    var multiplica = campo1 * campo2;
+    alert(multiplica)
+    document.getElementById('campo5').value='';
+    document.getElementById('campo6').value='';
+}
+function dividir(){
+    var campo1 = parseInt(document.getElementById("campo7").value);
+    var campo2 = parseInt(document.getElementById("campo8").value);
+
+    var divide = campo1 / campo2;
+    alert(divide)
+    document.getElementById('campo7').value='';
+    document.getElementById('campo8').value='';
+}
